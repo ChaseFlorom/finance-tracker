8ef8eeb91cc9bb809286a9d8fc3519e37658c2ca
florodude <chaseflorom@gmail.com>
2019-12-05 22:44:02 +0000

    Got financial code working

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -10,7 +10,7 @@ class Stock < ApplicationRecord
  
   def self.new_from_lookup(ticker_symbol)
     begin
-      client = IEX::Api::Client.new(publishable_token: 'pk_44af0d78512a4af79e9e9ac849d37873 ')
+      client = IEX::Api::Client.new(publishable_token: 'pk_44af0d78512a4af79e9e9ac849d37873')
       looked_up_stock = client.quote(ticker_symbol)
       new(name: looked_up_stock.company_name,
           ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   get 'my_portfolio', to: "users#my_portfolio"
+  get 'search_stocks', to: 'stocks#search'
   root 'welcome#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
