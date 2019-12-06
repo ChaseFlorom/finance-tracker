f0144c7f7c523395829d8f662da8ede741978cfa
florodude <chaseflorom@gmail.com>
2019-12-06 05:04:01 +0000

    ajax not working

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,23 @@
 class StocksController < ApplicationController 
    def search
-       @stock = Stock.new_from_lookup(params[:stock])
-       render 'users/my_portfolio'
+      if params[:stock].present?
+         @stock = Stock.new_from_lookup(params[:stock])
+         if @stock 
+            puts "should be"
+            respond_to do |format|
+            format.js { render partial: 'users/result' }            
+            end
+
+         else
+            puts "incorrect symbol"
+            flash[:danger] = "You have entered an incorrect symbol."
+            redirect_to my_portfolio_path
+         end
+          
+      else
+         puts "nothing"
+         flash[:danger] = "You need to type something, what did you expect to happen?"
+         redirect_to my_portfolio_path
+      end
    end
 end
